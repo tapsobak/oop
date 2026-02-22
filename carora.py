ec2e1c683ef57c5a1267b73e113df429d9b9f20c
Nasser Tapsoba <nassertapsoba@meta.com>
2026-02-22 10:41:07 -0500

    Created a Concession class to manage the cars

--- carora.py
+++ carora.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass  # we dont need a constructor with the dataclass decorator
@@ -26,13 +27,42 @@ class ElectricCars(Cars):
         return f"{parent_info}\nAutonomy Range: {self.range_miles} miles"
 
 
+class Concession:
+    def __init__(self, name, inventory=[]):
+        self.name = name
+        self.inventory = inventory
+
+    def add_car(self, car):
+        self.inventory.append(car)
+
+    def display_car_info(self):
+        print("--- Inventory ---")
+        for car in self.inventory:
+            print(f"\n{car}")
+
+    def sell_cars(self, make, model):
+        print("\n--- Sales ---")
+        for i in range(len(self.inventory)):
+            if make == self.inventory[i].make and model == self.inventory[i].model:
+                self.inventory.pop(i)
+                print(f"\nCar of make: {make} and model: {model} was sold.")
+                break
+            else:
+                print(
+                    f"\nCar of make: {make} and model: {model} was not found in inventory."
+                )
+                break
+
+
 # Instatiate
 camry = Cars("Toyota", "Camry", 35000, 9870)
 model3 = ElectricCars("Tesla", "Model 3", 45000, 3300, 450)
-
+fusion = Cars("Ford", "Fusion", 12500, 185000)
+c1 = Concession("Concession du Centre")
+c1.add_car(camry)
+c1.add_car(model3)
+c1.add_car(fusion)
 
 if __name__ == "__main__":
-    print("--- Gas Car ---")
-    print(camry)
-    print("\n--- Electric Car ---")
-    print(model3)
+    c1.display_car_info()
+    c1.sell_cars("Toyota", "Camry")
